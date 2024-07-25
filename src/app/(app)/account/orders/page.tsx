5b95ab3caf671a9419f43357d00219e0cef01e2e
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-07-25 14:57:03 +0530

    add order page

--- src/app/(app)/account/orders/page.tsx
+++ src/app/(app)/account/orders/page.tsx
@@ -1,5 +1,17 @@
+import AddressForm from "@/components/Accounts/AddressForm";
+import OrderCard from "@/components/orders/OrderCard";
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="py-10 w-full">
+      <h1 className="text-2xl font-semibold">Orders</h1>
+
+      <div className="grid grid-cols-1">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <OrderCard key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
