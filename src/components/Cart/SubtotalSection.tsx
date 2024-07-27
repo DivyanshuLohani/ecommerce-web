1bc3d8488a22fe9e8983a05766aa43afd924d8c4
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-07-27 12:41:19 +0530

    close cart when button clicked

--- src/components/Cart/SubtotalSection.tsx
+++ src/components/Cart/SubtotalSection.tsx
@@ -6,17 +6,22 @@ import React from "react";
 import { Button } from "../ui/button";
 
 export default function SubtotalSection() {
-  const { cartTotal } = useCart();
+  const { cartTotal, setCartOpen } = useCart();
   if (cartTotal <= 0) return null;
   return (
     <div className="absolute bottom-0 flex p-5 flex-col border-t w-full">
       <h5 className="text-lg font-semibold">Subtotal</h5>
       <span className="text-2xl">Rs. {formatCurrency(cartTotal)}</span>
       <div className="flex gap-2 mt-2 w-full">
-        <Button variant={"outline"} asChild className="w-1/2">
+        <Button
+          variant={"outline"}
+          asChild
+          className="w-1/2"
+          onClick={() => setCartOpen(false)}
+        >
           <Link href="/cart/">View Cart</Link>
         </Button>
-        <Button asChild className="w-1/2">
+        <Button asChild className="w-1/2" onClick={() => setCartOpen(false)}>
           <Link href="/checkout/">Checkout</Link>
         </Button>
       </div>
