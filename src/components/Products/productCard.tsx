eadb308c36f694e1f416c07cd0914dce0d909c23
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-09-25 12:30:37 +0530

    remove spring

--- src/components/Products/productCard.tsx
+++ src/components/Products/productCard.tsx
@@ -14,7 +14,8 @@ const cardVariants: Variants = {
     opacity: 1,
     y: 0,
     scale: 1,
-    transition: { type: "spring", stiffness: 100, damping: 10 },
+    // transition: { type: "spring", stiffness: 100, damping: 10 },
+    transition: { duration: 0.5 },
   },
 };
 export default function ProductCard({ product }: { product: Product }) {
