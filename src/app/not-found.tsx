5e9c0662cf711cc155412a9ed9798eb2763a348a
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-10-06 12:44:10 +0530

    Better 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 export default function notFound() {
   return (
-    <main className="p-4 md:p-6">
-      <div className="mb-8 space-y-4">
-        <h1 className="font-semibold text-lg md:text-2xl">Page not found</h1>
-        <p>The requested resource cannot be found on the server</p>
+    <div className="grid h-screen place-content-center bg-background px-4">
+      <div className="text-center">
+        <h1 className="text-9xl font-black text-muted">404</h1>
+
+        <p className="text-2xl font-bold tracking-tight text-muted-foreground sm:text-4xl">
+          Uh-oh!
+        </p>
+
+        <p className="mt-4 text-gray-500">We {"can't"} find that page.</p>
+
+        <Button size={"lg"} asChild className="mt-4">
+          <Link href="/">Go Back Home</Link>
+        </Button>
       </div>
-    </main>
+    </div>
   );
 }
